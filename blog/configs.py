9efcbd854845fe34edbe6498c7f62b5b9ef18884
topor <oleg_gromov@mail.ru>
2023-01-22 11:26:31 +0500

    Практическое задание №5

--- blog/configs.py
+++ blog/configs.py
@@ -4,9 +4,12 @@ import os
 class BaseConfig(object):
     DEBUG = False
     TESTING = False
-    SQLALCHEMY_DATABASE_URI = "sqlite:///:memory:"
+    basedir = os.path.abspath(os.path.dirname(__file__))
+    # SQLALCHEMY_DATABASE_URI = "sqlite:///:memory:"
+    # DATABASE_URL = "sqlite:///db.sqlite"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = "abcdefg123456"
+    WTF_CSRF_ENABLED = True
 
 
 class DevConfig(BaseConfig):
