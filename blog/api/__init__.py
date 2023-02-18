e19fb3cd06efb6c1e35557e0cfdef2ded42e6da9
topor-gromov <92143832+topor-gromov@users.noreply.github.com>
2023-02-18 19:24:55 +0500

    Update __init__.py

--- blog/api/__init__.py
+++ blog/api/__init__.py
@@ -39,8 +39,8 @@ def init_api(app):
 
     api.route(TagList, "tag_list", "/api/tags/", tag="Tag")
     api.route(TagDetail, "tag_detail", "/api/tags/<int:id>/", tag="Tag")
-    #api.route(UserList, "user_list", "/api/users/", tag="User")
-    #api.route(UserDetail, "user_detail", "/api/users/<int:id>/", tag="User")
+    api.route(UserList, "user_list", "/api/users/", tag="User")
+    api.route(UserDetail, "user_detail", "/api/users/<int:id>/", tag="User")
     #api.route(AuthorList, "author_list", "/api/authors/", tag="Author")
     #api.route(AuthorDetail, "author_detail", "/api/authors/<int:id>/", tag="Author")
     
